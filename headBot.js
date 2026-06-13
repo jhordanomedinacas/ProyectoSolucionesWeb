67de3b931a490b3b5e2ec1068d6159073338bd9f
jhordanMC <diego_mcj05@hotmail.com>
2026-06-13 05:36:02 -0500

    Update CHATBOT_API_URL to production endpoint

--- headBot.js
+++ headBot.js
@@ -32320,7 +32320,7 @@ class RGBELoader extends HDRLoader {
     super(manager);
   }
 }
-const CHATBOT_API_URL = "http://localhost:8000";
+const CHATBOT_API_URL = "https://jhordanmc-backendsolucioneswebml.hf.space";
 class HeadBot extends HTMLElement {
   connectedCallback() {
     const canvas = document.createElement("canvas");
